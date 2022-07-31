963387f0f7d418a76728f3fe8cc80001a39d4809
Leonardo Avanzi <47260167+leonardoavanzi@users.noreply.github.com>
2022-07-31 00:24:28 -0300

    tuple to object - example

--- src/tuple-to-object.ts
+++ src/tuple-to-object.ts
@@ -1,2 +1,13 @@
-const tuple = ["abcd", "mecatref", "robocop", "subzero"] as const
-console.log(typeof tuple[0])
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [TIndex in T[number]]: TIndex;
+}
+
+type SDAPFKAOP = TupleToObject<[10,20,30]>
+type ArrayMember = typeof tuple[number]
+
+const tuple = ["abcd", "mecatref", "robocop", "subzero"] as const;
+
+// @ts-expect-error
+type error = TupleToObject<[[1, "09321"], {age: 20}]>;
+
+
